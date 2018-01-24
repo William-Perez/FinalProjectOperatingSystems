d9c2b55ee11009f8edc93b520eb8f4f84484d492
JMontgomeryR <35747272+JMontgomeryR@users.noreply.github.com>
2018-01-24 14:56:22 -0400

    Add files via upload

--- disk.h
+++ disk.h
@@ -9,49 +9,55 @@ using namespace std;
 #ifndef DISK
 #define DISK
 
-class Disk {
+class Disk{
 private:
-	int numberOfTracks; // total number of tracks
-	int headStart; // position on the track where head started
-	int amountOfRequests; // total amount of request
-	int *requests; // pointer to array of the request
-	int *tracksTraversedBetween; // tracks traversed between request
-	bool direction; // direction of the head if true then head is up if false head is down
-	int averageSeekTime;
-	int *queue; //Ordered list
-	int queueSize;
-	bool scan = false;
+    int numberOfTracks; // total number of tracks
+    int headStart; // position on the track where head started
+    int amountOfRequests; // total amount of request
+    int *requests; // pointer to array of the request
+    int *tracksTraversedBetween; // tracks traversed between request
+    bool direction; // direction of the head if true then head is up if false head is down
+    int averageSeekTime;
+    int *queue;
+    int queueSize;
+    bool scan = false;
 public:
-	Disk();
-	Disk(int size);
-	void setData(Disk& disk);
-	void printTable(string algorithmName); // prints table like in the book
-	void addRequests(int requestAmount, int numberOfTracks);
-	void SSTF();
-	void SCAN();
-	void CSCAN();
-	void LOOK();
-	void CLOOK();
-	void Disk::calculateTracksTraversed(bool scan);
-	string convert_toString(int i);
-	void calculateSeekTime();
-	void arrivals();
-	~Disk();
+    Disk();
+    Disk(int size);
+    void setData();
+    void printTable(string algorithmName); // prints table like in the book
+    void addRequests(int requestAmount, int numberOfTracks);
+    string convert_toString(int i);
+    void calculateSeekTime();
+    int getAmountOfRequests();
+    void setHeadStart();
+    int getNumberOfTracks();
+    void setDirection();
+    void arrivals();
+	void FIFO();
+    void SSTF();
+  	void SCAN();
+  	void CSCAN();
+  	void LOOK();
+  	void CLOOK();
+  	void calculateTracksTraversed(bool scan);
+
+    ~Disk();
 };
 
-Disk::Disk() { }
+Disk::Disk(){ }
 
 Disk::Disk(int size)
 {
-	requests = new int[size];
-	tracksTraversedBetween = new int[size];
+    requests = new int [size];
+    tracksTraversedBetween = new int [size];
 }
 
 Disk::~Disk()
 {
-	delete[] requests;
-	delete[] tracksTraversedBetween;
-	delete[] queue;
+    delete[] requests;
+    delete[] tracksTraversedBetween;
+    delete[] queue;
 }
 
 void Disk::addRequests(int requestAmount, int numberOfTracks)
@@ -62,121 +68,149 @@ void Disk::addRequests(int requestAmount, int numberOfTracks)
 	{
 		cin >> number;
 		if (number >= 0 && number < numberOfTracks)
-		{
+        {
 			requests[i] = number;
 			i++;
-		}
-		else if (number < 0)
+        }
+		else if(number < 0)
 			cout << "Error invalid input! Keep adding positive integers.\n";
-		else
-			cout << "Cannot request a track that doesn't exist!\n";
+        else
+            cout << "Cannot request a track that doesn't exist!\n";
 	}
 }
 
-void Disk::setData(Disk& disk)
+void Disk::setData()
 {
-	int trackNum; // number of total tracks
-	int headPosition; // position of the track number
-	int requestNum; // number of request that will be input
-	int headMovement; // selection of which way the head will move
-	bool upwards;
+    int trackNum; // number of total tracks
+    int requestNum; // number of request that will be input
+    do{
+        cout << "How many tracks are on the disk? ";
+        cin >> trackNum;
+        if(trackNum <= 0)
+            cout << "Please use a positive non-zero integer for the amount of tracks. Try Again!\n";
+    }while(trackNum <= 0);
+    cout << endl;
+    numberOfTracks = trackNum;
 
-	do {
-		cout << "How many tracks are on the disk? ";
-		cin >> trackNum;
-		if (trackNum <= 0)
-			cout << "Please use a positive non-zero integer for the amount of tracks. Try Again!\n";
-	} while (trackNum <= 0);
-	cout << endl;
+    setHeadStart();
+    cout << endl;
 
-	do {
-		cout << "Enter the location of the head: ";
-		cin >> headPosition;
-		if (headPosition < 0)
-			cout << "Please use a non-negative integer for the head position. Try Again!\n";
-		else if (headPosition >= trackNum)
-			cout << "The position you are looking for does not exist in the disk. Try Again!\n";
-	} while (headPosition < 0 && headPosition >= trackNum);
-	cout << endl;
+    do{
+        cout << "Enter the amount of request that will be run: ";
+        cin >> requestNum;
+        if(requestNum <= 0)
+            cout << "Please a positive non-zero integer for the amount of request you will inputing. Try Again!\n";
+    }while(requestNum <= 0);
+    cout << endl;
+    amountOfRequests = requestNum;
+    requests = new int[requestNum];
+    addRequests(requestNum, trackNum);
 
-	do {
-		cout << "Enter the amount of request that will be run: ";
-		cin >> requestNum;
-		if (requestNum <= 0)
-			cout << "Please a positive non-zero integer for the amount of request you will inputing. Try Again!\n";
-	} while (requestNum <= 0);
-	cout << endl;
-	requests = new int[requestNum];
-	addRequests(requestNum, trackNum);
-
-	do {
-		cout << "In which direction is the head moving? Upwards or Downwards?\n";
-		cout << "1.Upwards\n";
-		cout << "2.Downwards\n";
-		cout << "Enter: ";
-		cin >> headMovement;
-
-		if (headMovement == 1)
-			upwards = true;
-		else if (headMovement == 2)
-			upwards = false;
-		else
-			cout << "Invalid Input! Try Again!\n";
-	} while (headMovement != 1 && headMovement != 2);
-	cout << endl;
-
-	numberOfTracks = trackNum;
-	headStart = headPosition;
-	amountOfRequests = requestNum;
-	queueSize = requestNum;
-	direction = upwards;
-	// tracksTraversedBetween = new int [requestNum];
+    setDirection();
+    cout << endl;
+    queueSize = requestNum;
 }
 
 void Disk::printTable(string algorithmName)
 {
-	stringstream track, travel;
-	TextTable t('-', '|', '+');
-	cout << setw(23) << algorithmName << endl;
-	t.add("Next Track Accessed");
-	t.add("Number of Tracks Traversed");
-	t.endOfRow();
+    stringstream track, travel;
+    TextTable t('-', '|', '+');
+    cout << setw(23) << algorithmName << endl;
+    t.add("Next Track Accessed");
+    t.add("Number of Tracks Traversed");
+    t.endOfRow();
 
-	for (int i = 0; i < amountOfRequests; i++)
-	{
-		t.add(convert_toString(requests[i]));
-		t.add(convert_toString(tracksTraversedBetween[i]));
-		t.endOfRow();
-	}
-	t.setAlignment(2, TextTable::Alignment::RIGHT);
-	cout << t;
-	cout << endl;
-	calculateSeekTime();
-	cout << "Average Seek Time: " << averageSeekTime << endl;
-	cout << "Press Enter to Continue!\n";
-	cin.get();
-	cin.get();
-	cout << endl;
+    for(int i = 0; i < queueSize; i++)
+    {
+        t.add(convert_toString(queue[i]));
+        t.add(convert_toString(tracksTraversedBetween[i]));
+        t.endOfRow();
+    }
+    t.setAlignment( 2, TextTable::Alignment::RIGHT );
+    cout << t;
+    cout << endl;
+    calculateSeekTime();
+    cout << "Average Seek Time: " << averageSeekTime << endl;
+    cout << "Press Enter to Continue!\n";
+    cin.get();
+    cin.get();
+    cout << endl;
 }
 
 string Disk::convert_toString(int i)
 {
-	stringstream ss;
-	ss << i;
-	return ss.str();
+    stringstream ss;
+    ss << i;
+    return ss.str();
 }
 
 void Disk::calculateSeekTime()
 {
-	int avg = 0;
+    int avg = 0;
 
-	for (int i = 0; i < amountOfRequests; i++)
+    for(int i = 0; i < amountOfRequests; i++)
+    {
+        avg += tracksTraversedBetween[i];
+    }
+
+    avg /= amountOfRequests;
+    averageSeekTime = avg;
+}
+
+int Disk::getAmountOfRequests()
+{
+    return amountOfRequests;
+}
+
+void Disk::setHeadStart()
+{
+    int headPosition; // position of the track number
+
+    do{
+        cout << "Enter the location of the head: ";
+        cin >> headPosition;
+        if(headPosition < 0)
+            cout << "Please use a non-negative integer for the head position. Try Again!\n";
+        else if(headPosition >= numberOfTracks)
+            cout << "The position you are looking for does not exist in the disk. Try Again!\n";
+    }while(headPosition < 0 && headPosition >= numberOfTracks);
+
+    headStart = headPosition;
+}
+
+int Disk::getNumberOfTracks()
+{
+    return numberOfTracks;
+}
+
+void Disk::setDirection()
+{
+    int headMovement;
+    bool upwards;
+    do{
+        cout << "In which direction is the head moving? Upwards or Downwards?\n";
+        cout << "1.Upwards\n";
+        cout << "2.Downwards\n";
+        cout << "Enter: ";
+        cin >> headMovement;
+
+        if(headMovement == 1)
+            upwards = true;
+        else if(headMovement == 2)
+            upwards = false;
+        else
+            cout << "Invalid Input! Try Again!\n";
+    }while(headMovement!= 1 && headMovement != 2);
+
+    direction = upwards;
+}
+
+void Disk::FIFO()
+{
+	for (int i = 0; i < amountOfRequests;i++)
 	{
-		avg += tracksTraversedBetween[i];
+		queue[i] = requests[i];
 	}
-
-	avg /= amountOfRequests;
-	averageSeekTime = avg;
 }
 
 void Disk::SSTF()
@@ -510,14 +544,14 @@ void Disk::calculateTracksTraversed(bool scan)
 				{
 					if (requests[i - 1] > requests[i])
 					{
-						tracksTraversedBetween[i] += (2 * (headStart - requests[i - 1]));
+						tracksTraversedBetween[i] += (2 * abs(headStart - requests[i - 1]));
 					}
 				}
 				else
 				{
 					if (requests[i - 1] < requests[i])
 					{
-						tracksTraversedBetween[i] += (2 * (headStart - requests[i - 1]));
+						tracksTraversedBetween[i] += (2 * abs(headStart - requests[i - 1]));
 					}
 				}
 			}
@@ -549,3 +583,4 @@ void Disk::arrivals()
 }
 
 #endif // DISK
+
