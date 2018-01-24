8baccc5d7f3e7c9a27fc7173cdd2d84762c1be55
William-Perez <35746875+William-Perez@users.noreply.github.com>
2018-01-24 10:28:08 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,29 +4,72 @@
 
 using namespace std;
 
-/*void FIFO(int *q, int start, int amount, int trackamount)
-{
-	cout << "Request | Tracks Traveled\n";
-	cout << q[0]<<" | "<< q[0] - start<<endl;
-	for (int i = 1; i < amount;i++)
-	{
-		cout << q[i] << " | " << abs(q[i-1] - q[i])<<endl;
-	}
-}
-
-void SSTF(int *q, int start)
-{
-	//int tracks[];
-
-}
-*/
 
 
 int main()
 {
+    int option;
     Disk userDisk;
-	userDisk.setData(userDisk);
-	userDisk.printTable("FIFO");
+	userDisk.setData();
+	do{
+        cout << "Welcome to the menu!\n";
+        cout << "1. Enter New Disk\n";
+        cout << "2. Change Head Position\n";
+        cout << "3. Change Requests\n";
+        cout << "4. Change Head Direction\n";
+        cout << "5. Print Out Tables For Algorithms\n";
+        cout << "10.End Program\n";
+        cout << "Enter: ";
+        cin >> option;
+        cout << endl;
+        switch(option)
+        {
+        case 1:
+            {
+                userDisk.setData();
+                break;
+            }
+        case 2:
+            {
+                userDisk.setHeadStart();
+                break;
+            }
+        case 3:
+            {
+                cout << "Remeber that you can add " << userDisk.getAmountOfRequests() << "requests.\n";
+                cout << "In a disk that has " << userDisk.getNumberOfTracks() << " total tracks.\n" << endl;
+                userDisk.addRequests(userDisk.getAmountOfRequests(), userDisk.getNumberOfTracks());
+                break;
+            }
+        case 4:
+            {
+                userDisk.setDirection();
+                break;
+            }
+        case 5:
+            {
+
+            }
+        case 10:
+            {
+                std::cout << std::endl;
+                std::cout << "Quitting...\n" << std::endl;
+                break;
+            }
+        default:
+            {
+                std::cout << std::endl;
+                if (std::cin.fail()) {
+                    std::cin.clear();
+                    std::cin.ignore();
+                    std::cout << "Option not valid. Please try again.\n";
+                } else {
+                    std::cout << "Option not valid. Please try again.\n";
+                }
+                std::cout << std::endl;
+            }
+        }
+	}while(option != 10);
 
 	return 0;
 }
